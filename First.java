2d8c58600af392a08836733f8aaf1e5ffc70565c
Jayalakshmi <jai.bellala@gmail.com>
2025-06-05 20:03:00 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,8 @@
+import java.util.Hashset;
+Public class StudentHashset{
+	Public Static void main(String[]args)
+	HashSet<String> studens=new HashSet<String>();
+	Scanner sc= new Scanner(System.in);
+	System.out.println("enter names pf 5 students")
+	while(students.size<5)
+}
